89e087607316c4dc8a4472cd2449e7d83d7781d6
sysstl <47250500+sysstl@users.noreply.github.com>
2024-04-07 16:16:09 +0300

    Update SLAE.h

--- SLAE.h
+++ SLAE.h
@@ -7,37 +7,6 @@ typedef std::vector<std::vector<double>> VecVecD;
 #define EPS ((double)(pow(10, -14)))
 
 class SLAE;
-class Matrixx;
-
-class Matrixx
-{
-public:
-	Matrixx(size_t Rows = 0, size_t Cols = 0);
-	Matrixx(VecVecD& values);
-	Matrixx(VecD values, bool gorizontal = true);
-	size_t GetRows();
-	size_t GetCols();
-	//double GetDeterminant();
-	Matrixx GetTransposed();
-	Matrixx GetReverse();
-	void SwitchRows(size_t Which, size_t Where);
-	size_t GetIndexOfBiggestValue(size_t Row, size_t Col);
-	double GetNorma();
-
-	//---------------------------//
-
-	Matrixx operator-(const Matrixx& other);
-	Matrixx operator*(const Matrixx& other);
-	VecD& operator[](size_t Index);
-
-private:
-
-	size_t MakeItTriangle();
-	//---------------------------//
-	VecVecD _Values;
-	size_t _Rows;
-	size_t _Cols;
-};
 
 class SLAE
 {
