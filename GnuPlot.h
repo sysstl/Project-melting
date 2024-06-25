d49760139db3b5daead47fb90fcde7540fc1f2c1
sysstl <47250500+sysstl@users.noreply.github.com>
2024-06-25 15:27:57 +0300

    Update GnuPlot.h

--- GnuPlot.h
+++ GnuPlot.h
@@ -22,48 +22,48 @@ class GnuPlot {
 
 	string GetGPPath();
 	string GetGPTerminal();
-	size_t GetTermnalWidth(); //ширина экрана 
+	size_t GetTermnalWidth(); //С€РёСЂРёРЅР° СЌРєСЂР°РЅР° 
 
 public:
-	//  передаем размеры области и зачение функций в размером виде (иселючение: fun в SetDataOnPlotColor3D)!!!!!
+	//  РїРµСЂРµРґР°РµРј СЂР°Р·РјРµСЂС‹ РѕР±Р»Р°СЃС‚Рё Рё Р·Р°С‡РµРЅРёРµ С„СѓРЅРєС†РёР№ РІ СЂР°Р·РјРµСЂРѕРј РІРёРґРµ (РёСЃРµР»СЋС‡РµРЅРёРµ: fun РІ SetDataOnPlotColor3D)!!!!!
 
-	// конструктор для создания плотиков в которых будем отображать данные полученные здесь в проекте и лежащие в массивах
-	GnuPlot(int number_of_plots_); // наш объект состоит из нескольких плотов // ++++++++++++++++++
-	// конструктор для создания плотиков в которых будем отображать экспериментальные данные лежащие в файлах изначально
+	// РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РґР»СЏ СЃРѕР·РґР°РЅРёСЏ РїР»РѕС‚РёРєРѕРІ РІ РєРѕС‚РѕСЂС‹С… Р±СѓРґРµРј РѕС‚РѕР±СЂР°Р¶Р°С‚СЊ РґР°РЅРЅС‹Рµ РїРѕР»СѓС‡РµРЅРЅС‹Рµ Р·РґРµСЃСЊ РІ РїСЂРѕРµРєС‚Рµ Рё Р»РµР¶Р°С‰РёРµ РІ РјР°СЃСЃРёРІР°С…
+	GnuPlot(int number_of_plots_); // РЅР°С€ РѕР±СЉРµРєС‚ СЃРѕСЃС‚РѕРёС‚ РёР· РЅРµСЃРєРѕР»СЊРєРёС… РїР»РѕС‚РѕРІ // ++++++++++++++++++
+	// РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РґР»СЏ СЃРѕР·РґР°РЅРёСЏ РїР»РѕС‚РёРєРѕРІ РІ РєРѕС‚РѕСЂС‹С… Р±СѓРґРµРј РѕС‚РѕР±СЂР°Р¶Р°С‚СЊ СЌРєСЃРїРµСЂРёРјРµРЅС‚Р°Р»СЊРЅС‹Рµ РґР°РЅРЅС‹Рµ Р»РµР¶Р°С‰РёРµ РІ С„Р°Р№Р»Р°С… РёР·РЅР°С‡Р°Р»СЊРЅРѕ
 	GnuPlot(int number_of_plots_, vector<string> filename_);
 
-	// Подготовка плота для отображения графика(ов) y(x)
-	void SetParametrs2D(int Current_number_plot, int number_of_lines, int width_line, string title_plot, string xlabel, string ylabel); // +++ проверить а риеал даных  // Current_number_plot=0,1,2, ....
-	// Подготовка плота для отображения данных цветовой палитрой (радуга)
+	// РџРѕРґРіРѕС‚РѕРІРєР° РїР»РѕС‚Р° РґР»СЏ РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ РіСЂР°С„РёРєР°(РѕРІ) y(x)
+	void SetParametrs2D(int Current_number_plot, int number_of_lines, int width_line, string title_plot, string xlabel, string ylabel); // +++ РїСЂРѕРІРµСЂРёС‚СЊ Р° СЂРёРµР°Р» РґР°РЅС‹С…  // Current_number_plot=0,1,2, ....
+	// РџРѕРґРіРѕС‚РѕРІРєР° РїР»РѕС‚Р° РґР»СЏ РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ РґР°РЅРЅС‹С… С†РІРµС‚РѕРІРѕР№ РїР°Р»РёС‚СЂРѕР№ (СЂР°РґСѓРіР°)
 	void SetParametrsOnPlotColor(int Current_number_plot, string title_plot, string xlabel, string ylabel, long float right_bondary_x, long float top_bondary_y); // Current_number_plot=0,1,2, ....
 
-	// эту функцию нужно вызвать только 1 раз перед SetDataOnPlot2D (эти 2-у ф-ции идут в свзке с SetDataOnPlot1D(2D, 3D))
-	// потребость этих 2-ух ф-ций: ф-ция SetDataOnPlot3D вызывается в цикле по времени + сохранить структуру данных при записи в файл
+	// СЌС‚Сѓ С„СѓРЅРєС†РёСЋ РЅСѓР¶РЅРѕ РІС‹Р·РІР°С‚СЊ С‚РѕР»СЊРєРѕ 1 СЂР°Р· РїРµСЂРµРґ SetDataOnPlot2D (СЌС‚Рё 2-Сѓ С„-С†РёРё РёРґСѓС‚ РІ СЃРІР·РєРµ СЃ SetDataOnPlot1D(2D, 3D))
+	// РїРѕС‚СЂРµР±РѕСЃС‚СЊ СЌС‚РёС… 2-СѓС… С„-С†РёР№: С„-С†РёСЏ SetDataOnPlot3D РІС‹Р·С‹РІР°РµС‚СЃСЏ РІ С†РёРєР»Рµ РїРѕ РІСЂРµРјРµРЅРё + СЃРѕС…СЂР°РЅРёС‚СЊ СЃС‚СЂСѓРєС‚СѓСЂСѓ РґР°РЅРЅС‹С… РїСЂРё Р·Р°РїРёСЃРё РІ С„Р°Р№Р»
 	void SetGridOnPlot2D(int Current_number_plot, int Nx, int Ny, double dx, double dy, int number_of_lines, Profile prof);
-	// эту функцию нужно вызвать только 1 раз перед SetDataOnPlot3D (эти 2-у ф-ции идут в свзке с SetDataOnPlot1D(2D, 3D))
-	// потребость этих 2-ух ф-ций: ф-ция SetDataOnPlot3D вызывается в цикле по времени + сохранить структуру данных при записи в файл
+	// СЌС‚Сѓ С„СѓРЅРєС†РёСЋ РЅСѓР¶РЅРѕ РІС‹Р·РІР°С‚СЊ С‚РѕР»СЊРєРѕ 1 СЂР°Р· РїРµСЂРµРґ SetDataOnPlot3D (СЌС‚Рё 2-Сѓ С„-С†РёРё РёРґСѓС‚ РІ СЃРІР·РєРµ СЃ SetDataOnPlot1D(2D, 3D))
+	// РїРѕС‚СЂРµР±РѕСЃС‚СЊ СЌС‚РёС… 2-СѓС… С„-С†РёР№: С„-С†РёСЏ SetDataOnPlot3D РІС‹Р·С‹РІР°РµС‚СЃСЏ РІ С†РёРєР»Рµ РїРѕ РІСЂРµРјРµРЅРё + СЃРѕС…СЂР°РЅРёС‚СЊ СЃС‚СЂСѓРєС‚СѓСЂСѓ РґР°РЅРЅС‹С… РїСЂРё Р·Р°РїРёСЃРё РІ С„Р°Р№Р»
 	void SetGridOnPlot3D(int Current_number_plot, int Nx, int Ny, int Nz, double dx, double dy, double dz, int number_of_lines, Profile prof);
 
-	// 1D, 2D, 3D - размерность задачи
-   ///(зависимости эксперим данных) y(x) (Ce от Te) (подумать над методамми которые бы отрисовывали даные лежащие уже в файлах(не полученные в проге))
-   // Для получения графика для 1D - задача тепл (U(x), U(t))  
+	// 1D, 2D, 3D - СЂР°Р·РјРµСЂРЅРѕСЃС‚СЊ Р·Р°РґР°С‡Рё
+   ///(Р·Р°РІРёСЃРёРјРѕСЃС‚Рё СЌРєСЃРїРµСЂРёРј РґР°РЅРЅС‹С…) y(x) (Ce РѕС‚ Te) (РїРѕРґСѓРјР°С‚СЊ РЅР°Рґ РјРµС‚РѕРґР°РјРјРё РєРѕС‚РѕСЂС‹Рµ Р±С‹ РѕС‚СЂРёСЃРѕРІС‹РІР°Р»Рё РґР°РЅС‹Рµ Р»РµР¶Р°С‰РёРµ СѓР¶Рµ РІ С„Р°Р№Р»Р°С…(РЅРµ РїРѕР»СѓС‡РµРЅРЅС‹Рµ РІ РїСЂРѕРіРµ))
+   // Р”Р»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РіСЂР°С„РёРєР° РґР»СЏ 1D - Р·Р°РґР°С‡Р° С‚РµРїР» (U(x), U(t))  
 	void SetDataOnPlot1D(int Current_number_plot, int Nx, double dx, double** fun_dimensionless, double parametr_for_dimension, int moment_of_time, double dt, Profile prof);
-	// Для получения графика функции (y(x)) для 2D - задача тепл (U(x), U(y), U(t))  
-	void SetDataOnPlot2D(int Current_number_plot, int Nx, int Ny, double dx, double dy, double** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis_x, int fixed_point_on_axis_y, int number_of_lines, int current_number_of_line, double moment_of_time, vector<double**> vec, Profile prof);// аналоги нижних двух функций, только пприменительо к графикам y(x), а не красочным
-	// Для получения графика функции (y(x) при серии t) для 3D - задача тепл (U(x), U(y), U(z),U(t)) 
-	void SetDataOnPlot3D(int Current_number_plot, int Nx, int Ny, int Nz, double dx, double dy, double dz, double*** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis_x, int fixed_point_on_axis_y, int fixed_point_on_axis_z, int number_of_lines, int current_number_of_line, double moment_of_time, vector<double***> vec, Profile prof); // linetype и поглдывать на рафики с задачи (от времеи могут быть)
+	// Р”Р»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РіСЂР°С„РёРєР° С„СѓРЅРєС†РёРё (y(x)) РґР»СЏ 2D - Р·Р°РґР°С‡Р° С‚РµРїР» (U(x), U(y), U(t))  
+	void SetDataOnPlot2D(int Current_number_plot, int Nx, int Ny, double dx, double dy, double** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis_x, int fixed_point_on_axis_y, int number_of_lines, int current_number_of_line, double moment_of_time, vector<double**> vec, Profile prof);// Р°РЅР°Р»РѕРіРё РЅРёР¶РЅРёС… РґРІСѓС… С„СѓРЅРєС†РёР№, С‚РѕР»СЊРєРѕ РїРїСЂРёРјРµРЅРёС‚РµР»СЊРѕ Рє РіСЂР°С„РёРєР°Рј y(x), Р° РЅРµ РєСЂР°СЃРѕС‡РЅС‹Рј
+	// Р”Р»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РіСЂР°С„РёРєР° С„СѓРЅРєС†РёРё (y(x) РїСЂРё СЃРµСЂРёРё t) РґР»СЏ 3D - Р·Р°РґР°С‡Р° С‚РµРїР» (U(x), U(y), U(z),U(t)) 
+	void SetDataOnPlot3D(int Current_number_plot, int Nx, int Ny, int Nz, double dx, double dy, double dz, double*** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis_x, int fixed_point_on_axis_y, int fixed_point_on_axis_z, int number_of_lines, int current_number_of_line, double moment_of_time, vector<double***> vec, Profile prof); // linetype Рё РїРѕРіР»РґС‹РІР°С‚СЊ РЅР° СЂР°С„РёРєРё СЃ Р·Р°РґР°С‡Рё (РѕС‚ РІСЂРµРјРµРё РјРѕРіСѓС‚ Р±С‹С‚СЊ)
 
-	// Для получения красочного рисунка (поле температуры пластины 2D)
+	// Р”Р»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РєСЂР°СЃРѕС‡РЅРѕРіРѕ СЂРёСЃСѓРЅРєР° (РїРѕР»Рµ С‚РµРјРїРµСЂР°С‚СѓСЂС‹ РїР»Р°СЃС‚РёРЅС‹ 2D)
 	void SetDataOnPlotColor2D(int Current_number_plot, int Nx, int Ny, double dx, double dy, double** fun, double parametr_for_dimension);
-	// Для получения красочного рисунка (профиль температуры в сечении (плоскости) 3-х мерного объекта - куба)
+	// Р”Р»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РєСЂР°СЃРѕС‡РЅРѕРіРѕ СЂРёСЃСѓРЅРєР° (РїСЂРѕС„РёР»СЊ С‚РµРјРїРµСЂР°С‚СѓСЂС‹ РІ СЃРµС‡РµРЅРёРё (РїР»РѕСЃРєРѕСЃС‚Рё) 3-С… РјРµСЂРЅРѕРіРѕ РѕР±СЉРµРєС‚Р° - РєСѓР±Р°)
 	void SetDataOnPlotColor3D(int Current_number_plot, int Nx, int Ny, int Nz, double dx, double dy, double dz, double*** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis, Plane plane);
 
-	// Отриосвка данных линиями (если есть много линий то легенда) 
+	// РћС‚СЂРёРѕСЃРІРєР° РґР°РЅРЅС‹С… Р»РёРЅРёСЏРјРё (РµСЃР»Рё РµСЃС‚СЊ РјРЅРѕРіРѕ Р»РёРЅРёР№ С‚Рѕ Р»РµРіРµРЅРґР°) 
 	void ShowDataOnPlot2D(int Current_number_plot, int number_of_lines, vector<string> list_name_line, string name_of_file, bool png_);
-	// Отриосвка данных цветовой палитрой (радуга)
+	// РћС‚СЂРёРѕСЃРІРєР° РґР°РЅРЅС‹С… С†РІРµС‚РѕРІРѕР№ РїР°Р»РёС‚СЂРѕР№ (СЂР°РґСѓРіР°)
 	void ShowDataOnPlotColor(int Current_number_plot, string name_of_file, bool png_);
-	// нужно если отрисовка идет во времени кадрами (при повтонром открытии удалются старые даннные)
-	void Close_and_open_files_for_replot(vector<int> Current_number_of_plots_); //Current_number_of_plots_ - вектор хранщий конкретные номера плотов которые нужно закрыть и сразу открыть
+	// РЅСѓР¶РЅРѕ РµСЃР»Рё РѕС‚СЂРёСЃРѕРІРєР° РёРґРµС‚ РІРѕ РІСЂРµРјРµРЅРё РєР°РґСЂР°РјРё (РїСЂРё РїРѕРІС‚РѕРЅСЂРѕРј РѕС‚РєСЂС‹С‚РёРё СѓРґР°Р»СЋС‚СЃСЏ СЃС‚Р°СЂС‹Рµ РґР°РЅРЅРЅС‹Рµ)
+	void Close_and_open_files_for_replot(vector<int> Current_number_of_plots_); //Current_number_of_plots_ - РІРµРєС‚РѕСЂ С…СЂР°РЅС‰РёР№ РєРѕРЅРєСЂРµС‚РЅС‹Рµ РЅРѕРјРµСЂР° РїР»РѕС‚РѕРІ РєРѕС‚РѕСЂС‹Рµ РЅСѓР¶РЅРѕ Р·Р°РєСЂС‹С‚СЊ Рё СЃСЂР°Р·Сѓ РѕС‚РєСЂС‹С‚СЊ
 	void Close_all_files_and_plots(int number_of_plots_); // ++++++++++++++++++
 
 };
