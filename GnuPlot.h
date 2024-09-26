ddfde37a8f9f502c6deb8abf268f6b3faa0ee234
sysstl <47250500+sysstl@users.noreply.github.com>
2024-09-26 14:19:05 +0300

    Update GnuPlot.h

--- GnuPlot.h
+++ GnuPlot.h
@@ -31,6 +31,8 @@ public:
 	GnuPlot(int number_of_plots_); // наш объект состоит из нескольких плотов // ++++++++++++++++++
 	// конструктор для создания плотиков в которых будем отображать экспериментальные данные лежащие в файлах изначально
 	GnuPlot(int number_of_plots_, vector<string> filename_);
+	// конструктор для создания 1 плота, который покажет данные с разных файлов (+ данные имеют разные сетки)
+	GnuPlot(vector<string> filename_);
 
 	// Подготовка плота для отображения графика(ов) y(x)
 	void SetParametrs2D(int Current_number_plot, int number_of_lines, int width_line, string title_plot, string xlabel, string ylabel); // +++ проверить а риеал даных  // Current_number_plot=0,1,2, ....
@@ -49,17 +51,21 @@ public:
    // Для получения графика для 1D - задача тепл (U(x), U(t))  
 	void SetDataOnPlot1D(int Current_number_plot, int Nx, double dx, double** fun_dimensionless, double parametr_for_dimension, int moment_of_time, double dt, Profile prof);
 	// Для получения графика функции (y(x)) для 2D - задача тепл (U(x), U(y), U(t))  
-	void SetDataOnPlot2D(int Current_number_plot, int Nx, int Ny, double dx, double dy, double** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis_x, int fixed_point_on_axis_y, int number_of_lines, int current_number_of_line, double moment_of_time, vector<double**> vec, Profile prof);// аналоги нижних двух функций, только пприменительо к графикам y(x), а не красочным
+	void SetDataOnPlot2D(int Current_number_plot, double** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis_x, int fixed_point_on_axis_y, int number_of_lines, int current_number_of_line, double moment_of_time, vector<double**> vec, Profile prof);// аналоги нижних двух функций, только пприменительо к графикам y(x), а не красочным
 	// Для получения графика функции (y(x) при серии t) для 3D - задача тепл (U(x), U(y), U(z),U(t)) 
-	void SetDataOnPlot3D(int Current_number_plot, int Nx, int Ny, int Nz, double dx, double dy, double dz, double*** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis_x, int fixed_point_on_axis_y, int fixed_point_on_axis_z, int number_of_lines, int current_number_of_line, double moment_of_time, vector<double***> vec, Profile prof); // linetype и поглдывать на рафики с задачи (от времеи могут быть)
+	void SetDataOnPlot3D(int Current_number_plot, double*** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis_x, int fixed_point_on_axis_y, int fixed_point_on_axis_z, int number_of_lines, int current_number_of_line, double moment_of_time, vector<double***> vec, Profile prof); // linetype и поглдывать на рафики с задачи (от времеи могут быть)
 
 	// Для получения красочного рисунка (поле температуры пластины 2D)
 	void SetDataOnPlotColor2D(int Current_number_plot, int Nx, int Ny, double dx, double dy, double** fun, double parametr_for_dimension);
 	// Для получения красочного рисунка (профиль температуры в сечении (плоскости) 3-х мерного объекта - куба)
 	void SetDataOnPlotColor3D(int Current_number_plot, int Nx, int Ny, int Nz, double dx, double dy, double dz, double*** fun_dimensionless, double parametr_for_dimension, int fixed_point_on_axis, Plane plane);
 
-	// Отриосвка данных линиями (если есть много линий то легенда) 
+	// Отриосвка данных линиями (если есть много линий то легенда) (к одному плоту привязан 1 файл данных)
 	void ShowDataOnPlot2D(int Current_number_plot, int number_of_lines, vector<string> list_name_line, string name_of_file, bool png_);
+	// Отриосвка данных линиями (если есть много линий то легенда) (к одному плоту привязано несколько файлов с данными)
+	void ShowDataOnPlot2D(int Current_number_plot, int Current_number_filename, int number_of_lines, vector<string> list_name_line, string name_of_file, bool png_);
+
+
 	// Отриосвка данных цветовой палитрой (радуга)
 	void ShowDataOnPlotColor(int Current_number_plot, string name_of_file, bool png_);
 	// нужно если отрисовка идет во времени кадрами (при повтонром открытии удалются старые даннные)
